51ccb318144c1f2aa26fc5a12299b5e41a174ca2
adiaz-nts <adiaz@neural.com.do>
2022-08-10 18:49:06 -0400

    Csharp tutorial for iteration statement

---
+++ ForIteration/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("The number {0} was found", i);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
